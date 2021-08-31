738883ffdc3789e985dd4686e28bf83042115864
Asadullah Hil Galib <coderboygalif@gmail.com>
2021-09-01 04:10:27 +0600

    ok

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function searchEnter(evt) {
             // console.log(inputFiled.value);
             url()
             resultItem.style.display = 'flex';
-
+            // location.replace(location.resultItem);
             // Clear input Filed Value 
             inputFiled.value = '';
         }  else{
@@ -33,12 +33,15 @@ function clearBtnFunc() {
     inputFiled.value = '';
     clearBtn.style.display = 'none';
 }
+
 function clearValue() {
     if (inputFiled.value.length >= 0) {
         clearBtn.style.display = 'block'; 
      } else {
          clearBtn.style.display = 'none';
-     }     
+     }
+     
+    
 };
 
 function url() {
@@ -48,10 +51,11 @@ function url() {
     .then(data => displayData(data));
 }
 function displayData(name){
+    resultItem.innerHTML = '';
        if(name.meals !== null) {
           name.meals.forEach(meal => {
-              console.log(meal);
-            const item = document.createElement('div');
+            
+           const item = document.createElement('div');
            item.innerHTML = 
            `
            <img src="${meal.strMealThumb}" />
@@ -67,12 +71,12 @@ function displayData(name){
            </div>
            `;
            item.classList.add('meal-item')
-           resultItem.appendChild(item)
+           resultItem.appendChild(item);
+
          });
         } else {
            warningNoValue.style.display = 'block'
            resultItem.style.display = 'none';
            warningNoValue.innerText = 'Please give me a valid or Name of cooked food!'
         }
-
 }
